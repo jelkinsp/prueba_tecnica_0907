911770ce6a3c6159ac5019532d6774210fa90515
jose <jlluengoramos@gmail.com>
2021-07-10 19:46:06 +0200

    0.0.0: The configuration of the default database in django has been changed, now Postgresql has been added.

--- prueba_tecnica/settings.py
+++ prueba_tecnica/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'prueba_tecnica.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'astrodb',
+        'USER': 'jelkins',
+        'PASSWORD': 'root1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
