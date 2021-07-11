bd4436f95b94468e65a629fa2ebef2e6d374ce8a
jose <jlluengoramos@gmail.com>
2021-07-11 03:53:46 +0200

    0.1.0: 3 models are added: observatory, device and asteroid.

--- asteroid/admin.py
+++ asteroid/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from asteroid.models import Observatory, Device, Asteriod
+
+
+@admin.register(Observatory)
+class ObservatoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "observatory_code", "name")
+    ordering = ("-id",)
+
+@admin.register(Device)
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = ("id", "device_code", "device_resolution")
+    ordering = ("-id",)
+
+@admin.register(Asteriod)
+class AsteriodAdmin(admin.ModelAdmin):
+    list_display = ("id", "device_matrix", "date", "time")
+    ordering = ("-time",)
+
--- asteroid/models.py
+++ asteroid/models.py
@@ -1,3 +1,24 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class Observatory(models.Model):
+    id = models.URLField(primary_key=True, default=uuid.uuid4(), editable=False)
+    observatory_code = models.CharField(max_length=32, unique=True, null=False, blank=False)
+    name = models.CharField(max_length=32, null=False, blank=False)
+
+
+class Device(models.Model):
+    id = models.URLField(primary_key=True, default=uuid.uuid4(), editable=False)
+    device_code = models.CharField(max_length=32, unique=True, null=False, blank=False)
+    device_resolution = models.CharField(max_length=5, null=False, blank=False)
+    observatories = models.ManyToManyField(Observatory)
+
+
+class Asteriod(models.Model):
+    id = models.URLField(primary_key=True, default=uuid.uuid4(), editable=False)
+    device_matrix = models.CharField(max_length=64, null=False, blank=False, unique=True)
+    date = models.DateField(null=False, blank=False)
+    time = models.TimeField(null=False, blank=False)
+    devices = models.ManyToManyField(Device)
