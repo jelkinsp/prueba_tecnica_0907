e62d487951ec4f94411fe299d32f7031ebb4df64
jose <jlluengoramos@gmail.com>
2021-07-12 07:14:54 +0200

    0.1.0: Refactoring of the models and the view, adapting to the test specifications.

--- asteroid/models.py
+++ asteroid/models.py
@@ -5,8 +5,8 @@ from django.db import models
 
 class Sighting(models.Model):
     id = models.URLField(primary_key=True, default=uuid.uuid4, editable=False)
-    observatory_code = models.CharField(max_length=32, unique=True, null=False, blank=False)
-    device_code = models.CharField(max_length=32, unique=True, null=False, blank=False)
+    observatory_code = models.CharField(max_length=32, null=False, blank=False)
+    device_code = models.CharField(max_length=32, null=False, blank=False)
     device_resolution = models.CharField(max_length=5, null=False, blank=False)
     device_matrix = models.CharField(max_length=64, null=False, blank=False, unique=True)
     device_matrix_clear = models.CharField(max_length=64, null=False, blank=False)
--- asteroid/views.py
+++ asteroid/views.py
@@ -1,11 +1,19 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from asteroid.models import Sighting
-from asteroid.serializers import SightingSerializer
+from asteroid.serializers import SightingSerializer, SightingListSerializer
 
 
 class SightingViewSet(viewsets.ModelViewSet):
     serializer_class = SightingSerializer
     queryset = Sighting.objects.all()
+    http_method_names = ['get']
 
-    http_method_names = ["get", "post"]
+    @action(detail=True, methods=["get"], url_path="search_sighting")
+    def get_sighting(self, request, pk=None):
+        sighting = self.get_object()
+
+        sighting_serializer = SightingListSerializer(sighting)
+        return Response(sighting_serializer.data, status=status.HTTP_200_OK)
